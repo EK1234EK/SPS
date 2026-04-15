bf5bd99978677eab85a6b6024a552f2b26b66708
EK1234EK <149094482+EK1234EK@users.noreply.github.com>
2026-04-15 18:00:14 +0200

    Added initial try at real srp model

--- src/system_dynamics/SRP.py
+++ src/system_dynamics/SRP.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 
+
 def sail_attitude(sail_control, radiation_location):
     alpha = sail_control[0]  # Tilt angle
     gamma = sail_control[1]  # Clock angle
@@ -17,14 +18,14 @@ def sail_attitude(sail_control, radiation_location):
     n = math.cos(alpha) * d_1 + math.sin(alpha) * math.sin(gamma) * d_2 + math.sin(alpha) * math.cos(gamma) * d_3
     return d_1, d_2, d_3, n
 
+
 class Solar_pressure:
 
-    def __init__(self, base_acc = 1.7 * 10 ** (-5)):
+    def __init__(self, base_acc=1.7 * 10 ** (-5)):
         self.base_acc = base_acc
         self.radiation_location = [0, 0, 0]
 
         self.sail_control = [0, 0]
-
 
     def ideal_sail(self, radiation_location, state):
 
@@ -47,21 +48,54 @@ class Solar_pressure:
         # Taking into account the direction of the resulting acceleration:
 
         if np.dot(d_1, n) > 0:
-            acc = list(self.base_acc * (np.dot(-d_1, n))**2 * n)
+            acc = list(self.base_acc * (np.dot(-d_1, n)) ** 2 * n)
         else:
             acc = list(-self.base_acc * (np.dot(-d_1, n)) ** 2 * n)
         return acc
 
     def real_sail(self, radiation_location, state):
 
+        alpha = self.sail_control[0]
+        clock = self.sail_control[1]
+
+        # Parameter defintions:
+        P = 1  # Photonic pressure
+        A = 1  # Sail area
+        r = 0.9  # Fraction of incidence photons that is reflectd
+        s = 0.82  # Fraction of photons that experiences specular reflection
+        B_f = 0.79  # Scattered fraction, frontside
+        B_b = 0.67  # Scattered fraction, backside
+
+        epsilon_f = 0.03  # Emissivity front
+        epsilon_B = 0.6  # Emissivity back
+
+        m = 1  # Mass of the spacecraft
+
         d_1, d_2, d_3, n = sail_attitude(sail_control=self.sail_control, radiation_location=self.radiation_location)
+
+        """if np.dot(d_1, n) < 0:
+            n *= -1"""
 
         # Determining the t-vector
 
-        return [0, 0, 0]
+        incidence = np.array(state[0:3]) - np.array(self.radiation_location)
+        temp = np.cross(n, incidence)
+        t = np.cross(temp, n)
+        t = t / np.linalg.norm(t)
 
-    def solar_acceleration(self, state,):
+        # Determining the two pre-factors:
+        # Normal force
+        f_n = P * A * ((1 + r * s) * math.cos(alpha) ** 2 + B_f * (1 - s) * r * math.cos(alpha) + (1 - r) * (
+                    epsilon_f * B_f - epsilon_B - B_b) / (epsilon_f + epsilon_B)) * n
+
+        f_t = P * A * (1 - r*s)*math.cos(alpha)*math.sin(alpha) * t
+
+        acc = (f_n + f_t) / m
+        return acc
+
+    def solar_acceleration(self, state, ):
         return self.ideal_sail(self.radiation_location, state)
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -71,18 +105,19 @@ if __name__ == "__main__":
 
     SRP = Solar_pressure(base_acc=1)
     SRP.sail_control = [0, 0.4]
-    state= [1, 0, 0]
+    state = [1, 0, 0]
+
 
     def get_acc(tilt, clock):
         SRP.sail_control = [tilt, clock]
         acc = SRP.solar_acceleration(state=state)
         return acc
 
+
     tilt_angle = np.linspace(-math.pi, math.pi, 50)
     clock_angle = np.linspace(-math.pi, math.pi, 50)
 
     # acc_array = list(np.zeros(shape=(len(tilt_angle), len(clock_angle))))
-
 
     acc_x, acc_y, acc_z, TILT, CLOCK, ANGLE = (np.zeros(shape=(len(tilt_angle), len(clock_angle))) for _ in range(6))
 
@@ -96,8 +131,9 @@ if __name__ == "__main__":
             acc_y[i][j] = acc_vec[1]
             acc_z[i][j] = acc_vec[2]
 
-            ANGLE[i][j] = math.acos((np.dot(acc_vec, np.array(state) - np.array(SRP.radiation_location))) / (np.linalg.norm(np.array(acc_vec)) * np.linalg.norm(np.array(state) - np.array(SRP.radiation_location))))
-
+            ANGLE[i][j] = math.acos((np.dot(acc_vec, np.array(state) - np.array(SRP.radiation_location))) / (
+                    np.linalg.norm(np.array(acc_vec)) * np.linalg.norm(
+                np.array(state) - np.array(SRP.radiation_location))))
 
     fig_1 = plt.figure()
     fig_2 = plt.figure()
@@ -120,7 +156,6 @@ if __name__ == "__main__":
 
     axes = [ax_1, ax_2, ax_3, ax_4]
     for axis in axes:
-
         axis.set_xlabel("Tilt angle [rad]")
         axis.set_ylabel("Clock angle [rad]")
         axis.set_zlabel("Acceleration angle")
@@ -140,4 +175,3 @@ if __name__ == "__main__":
     ax_4.set_zlabel("Angle")
 
     plt.show()
-
